d7ee473461787eec1e991a1fdb90d543cc9a6cda
rafaschein <rafaschein@gmail.com>
2015-03-31 09:39:57 -0300

    implement database

--- app.py
+++ app.py
@@ -1,61 +1,69 @@
+#!/usr/bin/env python
 from flask import Flask, render_template, abort, request, redirect
+import MySQLdb
+import yaml
+
 app = Flask(__name__)
 
-tasks = [
-  {'id': 1,
-   'title': u'Buy groceries',
-   'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-   'done': False},
-  {'id': 2,
-   'title': u'Learn Python',
-   'description': u'Need to find a good Python tutorial on the web',
-   'done': False}]
+stream   = open("database.yaml", 'r')
+database = yaml.load(stream)
+
+conn = MySQLdb.connect (host   = database['mysql']['host'],
+                        user   = database['mysql']['user'],
+                        passwd = database['mysql']['passwd'],
+                        db     = database['mysql']['db'])
+cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+
 
 @app.route("/tasks")
 def index():
+  cursor.execute("SELECT * FROM tasks")
+  tasks = cursor.fetchall()
   return render_template("task/list.html", tasks=tasks)
+
 
 @app.route("/task/view/<int:task_id>", methods=['GET'])
 @app.route("/tarefa/ver/<int:task_id>", methods=['GET'])
 def get(task_id):
-  task = [task for task in tasks if task['id'] == task_id]
+  cursor.execute("SELECT * FROM tasks WHERE id = '%s'", (task_id))
+  task = cursor.fetchone()
   if len(task) == 0:
     abort(404)
-  return render_template("task/view.html", task=task[0])
+  return render_template("task/view.html", task=task)
+
 
 @app.route("/task/new/")
 def new():
   return render_template("task/new.html")
 
+
 @app.route("/task/create", methods=['POST'])
 def create():
-  task = {'id': tasks[-1]['id'] + 1,
-          'title': request.form['title'],
-          'description': request.form['description'],
-          'done': False}
-
-  tasks.append(task)
+  sql = "INSERT INTO tasks (title, description) VALUES (%s, %s)"
+  cursor.execute(sql, (request.form['title'], request.form['description']))
   return redirect("/tasks", code=302)
+
 
 @app.route("/task/edit/<int:task_id>", methods=['GET'])
 def edit(task_id):
-  task = [task for task in tasks if task['id'] == task_id]
-  return render_template("task/edit.html", task=task[0])
+  cursor.execute("SELECT * FROM tasks WHERE id = '%s'", (task_id))
+  task = cursor.fetchone()
+  return render_template("task/edit.html", task=task)
+
 
 @app.route("/task/update/<int:task_id>", methods=['GET', 'POST'])
 def update(task_id):
-  key = [int(i) for i, task in enumerate(tasks) if task['id'] == task_id]
-  tasks[key[0]]['title']       = request.form['title']
-  tasks[key[0]]['description'] = request.form['description']
+  sql = "UPDATE tasks SET title = %s, description = %s WHERE id = '%s'"
+  cursor.execute(sql, (request.form['title'], request.form['description'], task_id))
+  task = cursor.fetchone()
   return redirect("/tasks", code=302)
+
 
 @app.route("/task/remove/<int:task_id>", methods=['GET'])
 def remove(task_id):
-  task = [task for task in tasks if task['id'] == task_id]
-  if len(task) == 0:
-    abort(404)
-  tasks.remove(task[0])
+  cursor.execute("DELETE FROM tasks WHERE id = '%s'", (task_id))
   return redirect("/tasks", code=302)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=True)
